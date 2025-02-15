3899995d3ba4f6390027445f6f715c410055896f
auraalex21 <48390729+auraalex21@users.noreply.github.com>
2025-02-15 11:42:52 +0100

    ok

--- commands/clear.js
+++ commands/clear.js
@@ -1,45 +1,40 @@
 import { SlashCommandBuilder } from "discord.js";
 
-const commandName = "clear";
+export const data = new SlashCommandBuilder()
+  .setName("clear")
+  .setDescription("Effacer un certain nombre de messages")
+  .addIntegerOption((option) =>
+    option
+      .setName("nombre")
+      .setDescription("Le nombre de messages à effacer")
+      .setRequired(true)
+  );
 
-export default {
-  name: commandName,
-  data: new SlashCommandBuilder()
-    .setName(commandName)
-    .setDescription("Effacer un certain nombre de messages")
-    .addIntegerOption((option) =>
-      option
-        .setName("nombre")
-        .setDescription("Le nombre de messages à effacer")
-        .setRequired(true)
-    ),
+export async function execute(interaction) {
+  const roleId = "1339230333953904751";
+  const number = interaction.options.getInteger("nombre");
 
-  async execute(interaction) {
-    const roleId = "1339230333953904751";
-    const number = interaction.options.getInteger("nombre");
-
-    if (!interaction.member.roles.cache.has(roleId)) {
-      return interaction.reply({
-        content: ":x: Vous n'avez pas la permission d'utiliser cette commande.",
-        ephemeral: true,
-      });
-    }
-
-    if (number < 1 || number > 100) {
-      return interaction.reply({
-        content: ":x: Vous devez spécifier un nombre entre 1 et 100.",
-        ephemeral: true,
-      });
-    }
-
-    const messages = await interaction.channel.messages.fetch({
-      limit: number,
-    });
-    await interaction.channel.bulkDelete(messages, true);
-
-    interaction.reply({
-      content: `✅ ${number} message(s) supprimé(s).`,
+  if (!interaction.member.roles.cache.has(roleId)) {
+    return interaction.reply({
+      content: ":x: Vous n'avez pas la permission d'utiliser cette commande.",
       ephemeral: true,
     });
-  },
-};
+  }
+
+  if (number < 1 || number > 100) {
+    return interaction.reply({
+      content: ":x: Vous devez spécifier un nombre entre 1 et 100.",
+      ephemeral: true,
+    });
+  }
+
+  const messages = await interaction.channel.messages.fetch({
+    limit: number,
+  });
+  await interaction.channel.bulkDelete(messages, true);
+
+  interaction.reply({
+    content: `✅ ${number} message(s) supprimé(s).`,
+    ephemeral: true,
+  });
+}
