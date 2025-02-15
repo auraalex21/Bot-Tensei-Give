ccedd30bb4fab36be6f197d2b2f035e7f93891ac
auraalex21 <48390729+auraalex21@users.noreply.github.com>
2025-02-15 10:16:28 +0100

    ok

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from "url";
 import { Client, GatewayIntentBits, Collection } from "discord.js";
 import dotenv from "dotenv";
 import synchronizeSlashCommands from "discord-sync-commands";
-import { GiveawaysManager } from "discord-giveaways";
 
 dotenv.config();
 
@@ -21,94 +20,100 @@ const client = new Client({
   ],
 });
 
-client.giveawaysManager = new GiveawaysManager(client, {
-  storage: path.resolve(__dirname, "./giveaways.json"),
-  default: {
-    botsCanWin: false,
-    embedColor: "#FF0000",
-    reaction: "🎉",
-    lastChance: {
-      enabled: true,
-      content: "⚠️ **DERNIÈRE CHANCE POUR PARTICIPER !** ⚠️",
-      threshold: 10000,
+(async () => {
+  const { GiveawaysManager } = await import("discord-giveaways");
+
+  client.giveawaysManager = new GiveawaysManager(client, {
+    storage: path.resolve(__dirname, "./giveaways.json"),
+    default: {
+      botsCanWin: false,
       embedColor: "#FF0000",
+      reaction: "🎉",
+      lastChance: {
+        enabled: true,
+        content: "⚠️ **DERNIÈRE CHANCE POUR PARTICIPER !** ⚠️",
+        threshold: 10000,
+        embedColor: "#FF0000",
+      },
     },
-  },
-});
+  });
 
-client.giveawaysManager.on(
-  "giveawayReactionAdded",
-  (giveaway, member, reaction) => {
-    console.log(
-      `${member.user.tag} a participé au tirage au sort #${giveaway.messageId} (${reaction.emoji.name})`
-    );
-  }
-);
-
-client.giveawaysManager.on(
-  "giveawayReactionRemoved",
-  (giveaway, member, reaction) => {
-    console.log(
-      `${member.user.tag} a retiré sa réaction du tirage au sort #${giveaway.messageId} (${reaction.emoji.name})`
-    );
-  }
-);
-
-client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
-  console.log(
-    `Le tirage au sort #${giveaway.messageId} est terminé ! Gagnants : ${winners
-      .map((member) => member.user.username)
-      .join(", ")}`
-  );
-});
-
-client.commands = new Collection();
-
-const loadCommands = (dir) => {
-  const files = fs.readdirSync(dir);
-  for (const file of files) {
-    const filePath = path.join(dir, file);
-    if (fs.statSync(filePath).isDirectory()) {
-      loadCommands(filePath);
-    } else if (file.endsWith(".js")) {
-      import(path.resolve(filePath)).then((command) => {
-        if (command.data) {
-          client.commands.set(command.data.name, command);
-          console.log(`👌 Commande chargée : ${command.data.name}`);
-        } else {
-          console.error(
-            `❌ La commande dans ${filePath} n'a pas de nom défini.`
-          );
-        }
-      });
+  client.giveawaysManager.on(
+    "giveawayReactionAdded",
+    (giveaway, member, reaction) => {
+      console.log(
+        `${member.user.tag} a participé au tirage au sort #${giveaway.messageId} (${reaction.emoji.name})`
+      );
     }
-  }
-};
+  );
 
-loadCommands(path.resolve(__dirname, "./commands"));
+  client.giveawaysManager.on(
+    "giveawayReactionRemoved",
+    (giveaway, member, reaction) => {
+      console.log(
+        `${member.user.tag} a retiré sa réaction du tirage au sort #${giveaway.messageId} (${reaction.emoji.name})`
+      );
+    }
+  );
 
-synchronizeSlashCommands(
-  client,
-  client.commands.map((c) => c.data.toJSON()),
-  {
-    debug: true,
-    guildId: process.env.GUILD_ID,
-  }
-);
+  client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
+    console.log(
+      `Le tirage au sort #${
+        giveaway.messageId
+      } est terminé ! Gagnants : ${winners
+        .map((member) => member.user.username)
+        .join(", ")}`
+    );
+  });
 
-fs.readdir(path.resolve(__dirname, "./events/"), (_err, files) => {
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    import(path.resolve(__dirname, `./events/${file}`)).then((event) => {
-      let eventName = file.split(".")[0];
-      console.log(`👌 Événement chargé : ${eventName}`);
-      client.on(eventName, event.default.bind(null, client));
+  client.commands = new Collection();
+
+  const loadCommands = (dir) => {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+      const filePath = path.join(dir, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        loadCommands(filePath);
+      } else if (file.endsWith(".js")) {
+        import(path.resolve(filePath)).then((command) => {
+          if (command.data) {
+            client.commands.set(command.data.name, command);
+            console.log(`👌 Commande chargée : ${command.data.name}`);
+          } else {
+            console.error(
+              `❌ La commande dans ${filePath} n'a pas de nom défini.`
+            );
+          }
+        });
+      }
+    }
+  };
+
+  loadCommands(path.resolve(__dirname, "./commands"));
+
+  synchronizeSlashCommands(
+    client,
+    client.commands.map((c) => c.data.toJSON()),
+    {
+      debug: true,
+      guildId: process.env.GUILD_ID,
+    }
+  );
+
+  fs.readdir(path.resolve(__dirname, "./events/"), (_err, files) => {
+    files.forEach((file) => {
+      if (!file.endsWith(".js")) return;
+      import(path.resolve(__dirname, `./events/${file}`)).then((event) => {
+        let eventName = file.split(".")[0];
+        console.log(`👌 Événement chargé : ${eventName}`);
+        client.on(eventName, event.default.bind(null, client));
+      });
     });
   });
-});
 
-client.once("ready", () => {
-  console.log(`Prêt en tant que ${client.user.tag}`);
-});
+  client.once("ready", () => {
+    console.log(`Prêt en tant que ${client.user.tag}`);
+  });
 
-client.login(process.env.DISCORD_TOKEN);
+  client.login(process.env.DISCORD_TOKEN);
+})();
